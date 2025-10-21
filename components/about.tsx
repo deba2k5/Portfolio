c79c108e78e412a2b5070c2209ed3bcd08b78bd3
Debangshu Chatterjee <o.24.debangshu@gmail.com>
2025-10-21 22:39:48 +0530

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -37,10 +37,10 @@ export function About() {
             <p className="text-center">Problem Solver</p>
           </div>
           <div className="border border-pink-500 rounded-md p-3 bg-black/50 hover:bg-pink-500/10 transition-colors">
-            <p className="text-center">IoT Developer</p>
+            <p className="text-center">Full-stack Developer</p>
           </div>
           <div className="border border-blue-500 rounded-md p-3 bg-black/50 hover:bg-blue-500/10 transition-colors">
-            <p className="text-center">ML Explorer</p>
+            <p className="text-center">GenAI and ML Explorer</p>
           </div>
         </div>
       </div>
@@ -51,12 +51,24 @@ export function About() {
           <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-green-500"></div>
             <h4 className="text-lg font-medium text-white">Tech Intella JU 2024</h4>
-            <p className="text-gray-400">2nd Runner-Up with innovative project</p>
+            <p className="text-gray-400">2nd Runner-Up at Jadavpur University</p>
           </div>
 
           <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
-            <h4 className="text-lg font-medium text-white">AICTE IDE Bootcamp 2024 Winner</h4>
+            <h4 className="text-lg font-medium text-white">ICOE HACKATHON</h4>
+            <p className="text-gray-400">Rank-Top 50</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Sustain-A-Thon Indian Oil</h4>
+            <p className="text-gray-400">Rank-Top 120</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">AICTE IDE Bootcamp Phase 1 Winner</h4>
             <p className="text-gray-400">IIM Sambalpur - Startup pitch competition winner</p>
           </div>
 
@@ -67,27 +79,93 @@ export function About() {
           </div>
 
           <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">DevFest AI 2024</h4>
+            <p className="text-gray-400">Rank-34</p>
+          </div>
+
+          <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-yellow-500"></div>
             <h4 className="text-lg font-medium text-white">Smart India Hackathon 2024</h4>
-            <p className="text-gray-400">PSID 1528 Finalist</p>
+            <p className="text-gray-400">PSID 1528 Finalist AIR - Top 5</p>
           </div>
 
           <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-red-500"></div>
-            <h4 className="text-lg font-medium text-white">Yukti Udaymahotsav 2025</h4>
-            <p className="text-gray-400">Eastern Zone Top 30</p>
+            <h4 className="text-lg font-medium text-white">AICTE Yukti Udaymahotsav 2025</h4>
+            <p className="text-gray-400">Eastern Zone Top 30 Startups</p>
           </div>
 
           <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-blue-500"></div>
-            <h4 className="text-lg font-medium text-white">AICTE IDE Bootcamp 2025 Winner</h4>
-            <p className="text-gray-400">Amity University Ranchi</p>
+            <h4 className="text-lg font-medium text-white">AICTE IDE Bootcamp Phase 2 Winner</h4>
+            <p className="text-gray-400">Winner at Amity University Ranchi</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Kshitij B-Plan IIT KGP</h4>
+            <p className="text-gray-400">Rank-Top 10</p>
           </div>
 
           <div className="relative">
             <div className="absolute -left-6 w-4 h-4 rounded-full bg-pink-500"></div>
             <h4 className="text-lg font-medium text-white">Eureka Innovacia IEM 2025</h4>
             <p className="text-gray-400">Runners Up</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">ICYIM 2025</h4>
+            <p className="text-gray-400">Runners Up</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Hexafalls 2025</h4>
+            <p className="text-gray-400">PrepVerse Track Winner</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Infosys Global Hackathon 2025</h4>
+            <p className="text-gray-400">State Winner</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Infosys Global Hackathon 2025</h4>
+            <p className="text-gray-400">AIR-8</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">IEEE YESIST12 2025, Malaysia</h4>
+            <p className="text-gray-400">Recognised Top 3 Globally</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Tata Social Enterprise Challenge 2025</h4>
+            <p className="text-gray-400">Winner at TSEC 2025</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">AISC 2025</h4>
+            <p className="text-gray-400">Paper accepted and presented</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Taylor and Francis 2025</h4>
+            <p className="text-gray-400">Book Chapter Accepted</p>
+          </div>
+
+          <div className="relative">
+            <div className="absolute -left-6 w-4 h-4 rounded-full bg-cyan-500"></div>
+            <h4 className="text-lg font-medium text-white">Nova Publications Springer</h4>
+            <p className="text-gray-400">Paper Accepted for presentation</p>
           </div>
         </div>
       </div>
